6c543c268284e1a2b5a9a4fb79dfda93f835d166
Ron Rizzo <churchmouth13@gmail.com>
2024-08-14 10:18:55 -0400

    Add Survivor entitity
    Added Survivor entity and associated code.

---
+++ src/main/java/org/example/springresttest/Survivor.java
@@ -0,0 +1,62 @@
+package org.example.springresttest;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Survivor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+    private String lastName;
+    private String DOB;
+    private String Birthplace;
+    //private ArrayList<Vehicle> RegisteredVehicles;
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getBirthplace() {
+        return Birthplace;
+    }
+
+    public void setBirthplace(String birthplace) {
+        Birthplace = birthplace;
+    }
+
+    @Override
+    public String toString() {
+        return "Survivor{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", DOB='" + DOB + '\'' +
+                ", Birthplace='" + Birthplace + '\'' +
+                '}';
+    }
+}
