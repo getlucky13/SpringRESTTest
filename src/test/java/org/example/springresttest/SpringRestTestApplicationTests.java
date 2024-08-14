816ee2ac6129937e01d432aee4bd98b2abdb5ddf
Ron Rizzo <churchmouth13@gmail.com>
2024-08-14 09:27:36 -0400

    Initial commit

---
+++ src/test/java/org/example/springresttest/SpringRestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springresttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
